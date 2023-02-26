83ca050a6d61f0489866d979476c507d66aa337f
linaqou <lin777qou@gmail.com>
2023-02-26 15:08:33 +0400

    work version, add time, add songs playing

--- js/album.js
+++ js/album.js
@@ -30,13 +30,79 @@ for(let i = 0; i < tracks.length; i++){
     let track = tracks[i];
 
     playlist.innerHTML += `
-        <li class="list-group-item d-flex align-items-center">
-            <img src="assets/play-button.png" alt="" height="30px" class="me-3">
+        <li class="track list-group-item d-flex align-items-center">
+            <img src="assets/play-button.png" alt="" height="30px" class="play me-3">
+            <img src="assets/playing.png" height="30px" class="playing me-3 d-none">
             <div>
                 <div>${track.title}</div>
-                <div>${track.author}</div>
+                <div class="text-secondary">${track.author}</div>
             </div>
-            <div class="ms-auto">${track.time}</div>
+            <div class="time ms-auto">${track.time}</div>
+            <audio class="audio" src="${track.src}"></audio>
         </li>
     `
 }
+
+function setupAudio() {
+    // Найди коллекцию с треками
+
+    let trackNodes = document.querySelectorAll(`.track`); 
+    for (let i = 0; i < trackNodes.length; i++) { 
+        // Один элемент
+        let node = trackNodes[i]; 
+        let timeNode = node.querySelector(`.time`);
+        // Тег аудио внутри этого элемента
+        let audio = node.querySelector(`.audio`);
+
+        function updateProgress(isPlaying) {
+            // Нарисовать актуальное время
+            timeNode.innerHTML = getTime(audio.currentTime);
+            if (isPlaying) {
+                  requestAnimationFrame(updateProgress);
+            }
+        }
+    
+
+        let isPlaying = false;
+        node.addEventListener(`click`, function () {
+            let playButton = node.querySelector(`.play`);
+            let playingButton = node.querySelector(`.playing`);
+            // Если трек сейчас играет...
+            if (isPlaying) {
+                isPlaying = false;
+                // Поставить на паузу
+                playButton.classList.remove(`d-none`);
+                playingButton.classList.add(`d-none`);
+
+                audio.pause();
+
+            // Если трек сейчас не играет...
+            } else {
+                isPlaying = true;
+                // Включить проигрывание
+                playButton.classList.add(`d-none`);
+                playingButton.classList.remove(`d-none`);
+
+                audio.play();
+                updateProgress(isPlaying);
+            }
+        });
+    }
+}
+
+function getTime(time){
+    let currentSeconds = Math.floor(time);
+    let minutes = Math.floor(currentSeconds / 60);
+    let seconds = Math.floor(currentSeconds % 60);
+
+    if (minutes < 10){
+        minutes = `0` + minutes;
+    }
+    if (seconds < 10){
+        seconds = `0` + seconds;
+    }
+    
+    return `${minutes}:${seconds}`
+}
+
+setupAudio();
--- js/data.js
+++ js/data.js
@@ -7,29 +7,34 @@ let albums =
         year: `2022`,
         tracks: [
             {
-                title: `Трек 1`,
-                author: `Моцарт`,
-                time: `3:59`
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
             },
             {
-                title: `Трек 2`,
-                author: `Моцарт`,
-                time: `2:34`
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
             },
             {
-                title: `Трек 3`,
-                author: `Моцарт`,
-                time: `3:12`
+                title: `Bad to the Bone`,
+                author: `George`,
+                time: `4:50`,
+                src: `music/20  George Thorogood & The Destroyers - Bad To The Bone.mp3`
             },
             {
-                title: `Трек 4`,
-                author: `Моцарт`,
-                time: `3:28`
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
             },
             {
-                title: `Трек 5`,
-                author: `Моцарт`,
-                time: `1:29`
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
             }
         ]
     },
@@ -38,20 +43,116 @@ let albums =
         title: `Рок`,
         image: `assets/rock.jpg`,
         description: `Yeah baby`,
-        year: `2022`
+        year: `2022`,
+        tracks: [
+            {
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
+            },
+            {
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
+            },
+            {
+                title: `Bad to the Bone`,
+                author: `George`,
+                time: `4:50`,
+                src: `music/20  George Thorogood & The Destroyers - Bad To The Bone.mp3`
+            },
+            {
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
+            },
+            {
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
+            }
+        ]
     },
 
     {
         title: `Боевая музыка`,
         image: `assets/war.jpg`,
         description: `Nooo`,
-        year: `1941`
+        year: `1941`,
+        tracks: [
+            {
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
+            },
+            {
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
+            },
+            {
+                title: `Bad to the Bone`,
+                author: `George`,
+                time: `4:50`,
+                src: `music/20  George Thorogood & The Destroyers - Bad To The Bone.mp3`
+            },
+            {
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
+            },
+            {
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
+            }
+        ]
     },
 
     {
         title: `Соберись с мыслями`,
         image: `assets/calm.jpg`,
         description: `Calming music`,
-        year: `2023`
+        year: `2023`,
+        tracks: [
+            {
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
+            },
+            {
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
+            },
+            {
+                title: `Bad to the Bone`,
+                author: `George`,
+                time: `4:50`,
+                src: `music/20  George Thorogood & The Destroyers - Bad To The Bone.mp3`
+            },
+            {
+                title: `Highway to Hell`,
+                author: `AC/DC`,
+                time: `3:33`,
+                src: `music/AC ⁄ DC - Highway to Hell.mp3`
+            },
+            {
+                title: `Back in Black`,
+                author: `AC/DC`,
+                time: `4:12`,
+                src: `music/AC DC - Back In Black (OST Iron Man ).mp3`
+            }
+        ]
     },
 ]
